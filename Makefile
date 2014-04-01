fd419c74cd1cc42abafd4c562e1960df4f5a3bc3
范敦球 <fandunqiu@fandunqiu-desktop.(none)>
2014-04-01 18:13:41 +0800

    linux 0.00 test

--- Makefile
+++ Makefile
@@ -1,43 +1,33 @@
-AS86	= as86 -0 -a
-LD86	= ld86 -0
 
-AS		= as
-LD		= ld
-LDFLAGS	= --oformat binary -N -e startup_32 -Ttext 0x0000
+AS	 = as
+LD	 = ld
+LDFLAGS0 = -s -e start -Ttext 0x0000 --oformat binary
+LDFLAGS1 = -Ttext 0x0000 -e startup_32 --oformat binary
 
-.s.o:
-	$(AS) -o $*.o $<
-
-all: floppy.img
+disk: floppy.img
 
 floppy.img: Image
-	dd if=/dev/zero of=floppy.img bs=1024 count=1440
-	dd if=Image of=floppy.img bs=2048 count=1 conv=notrunc
+	dd if=/dev/zero of=bochs/floppy.img bs=1024 count=1440
+	dd if=Image of=bochs/floppy.img bs=2048 count=1 conv=notrunc
 	sync
 
-Image: boot/boot tools/system  tools/build
-	tools/build boot/boot tools/system  > Image
+all: Image
+
+Image: tools/boot tools/head
+	cat tools/boot boot/head > Image
 	sync
 
-boot/head.o: boot/head.s
+tools/boot: boot/boot.s
+	$(AS) -o boot/boot.o boot/boot.s
+	$(LD) $(LDFLAGS0) -o tools/boot boot/boot.o
 
-tools/build: tools/build.c
-	$(CC) $(CFLAGS) -o tools/build tools/build.c
-
-tools/system: boot/head.o
-	$(LD) $(LDFLAGS) boot/head.o -o tools/system > System.map
-
-boot/boot: boot/boot.s
-	$(AS86) -o boot/boot.o boot/boot.s
-	$(LD86) -s -o boot/boot boot/boot.o
+tools/head: boot/head.s
+	$(AS) -o boot/head.o boot/head.s
+	$(LD) $(LDFLAGS1) boot/head.o -o tools/head > tools/System.map
 
 clean:
 	rm -f boot/*.o
-	rm -f boot/boot
-	rm -f tools/build
-	rm -f tools/system
-	rm -f boot/head.o
-	rm -f boot/head
-	rm -f System.map
-	rm -f floppy.img
+	rm -f tools/System.map
+	rm -f tools/boot
+	rm -f tools/head
 	rm -f Image
--- boot/boot.s
+++ boot/boot.s
@@ -1,180 +1,71 @@
 
-sectors = 18
-
-.global begtext, begdata, begbss, endtext, enddata, endbss
-.text
-begtext:
-.data
-begdata:
-.bss
-begbss:
-.text
-
 BOOTSEG = 0x07c0
-INITSEG = 0x9000
 SYSSEG  = 0x1000
 SYSLEN  = 17
 
-entry start
+.code16
+.text
+
+.global start
 start:
-	mov ax, #BOOTSEG
-	mov ds, ax
-	mov ax, #INITSEG
-	mov es, ax
-	mov cx, #256
-	sub si,	si
-	sub di, di
-	rep
-	movw
-	jmpi go, INITSEG
-
+	ljmp $BOOTSEG, $go
 go:
-	mov ax, cs
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
-	mov sp, #0x400
-	
-	mov ah, #0x03
-	xor bh, bh
-	int 0x10
-
-	mov cx, #24
-	mov bx, #0x7
-	mov bp, #load_msg
-	mov ax, #0x1301
-	int 0x10
+	movw %cs, %ax
+	movw %ax, %ds
+	movw %ax, %ss
+	movw $0x400, %sp
 
 load_system:
-	mov	dx, #0x0000
-	mov	cx, #0x0002
-	mov	ax, #SYSSEG
-	mov	es, ax
-	mov	bx, #0x0000
-	mov	ax, #0x200+SYSLEN
-	int 0x13
-	jnc	ok_load
-	mov	dx, #0x0000
-	mov	ax, #0x0000
-	int	0x13
-	jmp	load_system
+	movw $0x0000, %dx
+	movw $0x0002, %cx
+	movw $SYSSEG, %ax
+	movw %ax, %es
+	xor  %bx, %bx
+	movw $0x211,  %ax
+	int  $0x13
+	jnc  ok_load
+die:
+	jmp die
 
 ok_load:
-	call kill_motor
-
-	mov	ah,#0x03
-	xor	bh,bh
-	int	0x10 
-	mov	[510],dx
-	
 	cli
-
-	mov	ax, #0x0000
-	cld
-
-do_move:
-	mov es, ax
-	add ax, #0x1000
-	cmp ax, #0x9000
-	jz end_move
-	mov ds, ax
-	sub di, di
-	sub si, si
-	mov cx, #0x200
-	rep
-	movsw
-	jmp do_move
-
-end_move:
-	mov ax, cs
-	mov ds, ax
+	movw $SYSSEG, %ax
+	movw %ax, %ds
+	xor  %ax, %ax
+	movw %ax, %es
+	movw $0x2000, %cx
+	sub  %si, %si
+	sub  %di, %di
+	rep  movsw
+	movw $BOOTSEG, %ax
+	movw %ax, %ds
 	lidt idt_48
 	lgdt gdt_48
-
-	call empty_8042
-	mov	al, #0xD1
-	out	#0x64, al
-	call empty_8042
-	mov	al, #0xDF
-	out	#0x60, al
-	call empty_8042
-
-
-	mov	al,#0x11
-	out	#0x20,al
-	.word	0x00eb,0x00eb
-	out	#0xA0,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x20
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x28
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x04
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x02
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0x01
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	out	#0xA1,al
-	.word	0x00eb,0x00eb
-	mov	al,#0xFF
-	out	#0x21,al
-	.word	0x00eb,0x00eb
-	out	#0xA1,al
-
-	mov	ax,#0x0001
-	lmsw ax
-	jmpi 0, 8
-
-empty_8042:
-	.word 0x00eb,0x00eb
-	in	 al,#0x64
-	test al,#2
-	jnz	 empty_8042
-	ret
-
-kill_motor:
-	push dx
-	mov  dx, #0x3f2
-	mov  al, #0
-	outb
-	pop  dx
-	ret
+	
+	movw $0x0001, %ax
+	lmsw %ax
+	ljmp $8,  $0
 
 gdt:
-	.word	0,0,0,0		; dummy
-
-	.word	0xFFFF		; 4G
-	.word	0x0000		; base address=0
-	.word	0x9A00		; code read/exec
-	.word	0x00C0		; granularity=4096, 386
-
-	.word	0xFFFF		; 4G
-	.word	0x0000		; base address=0
-	.word	0x9200		; data read/write
-	.word	0x00C0		; granularity=4096, 386
+	.word 0, 0, 0, 0 
+	
+	.word 0x07FF
+	.word 0x0000
+	.word 0x9A00
+	.word 0x00c0
+	
+	.word 0x07FF
+	.word 0x0000
+	.word 0x9200
+	.word 0x00c0
 
 idt_48:
-	.word	0			; idt limit=0
-	.word	0,0			; idt base=0L
+	.word 0
+	.word 0, 0
 
 gdt_48:
-	.word	0x800		; gdt limit=2048, 256 GDT entries
-	.word   gdt,0x9		; gdt base = 0X9xxxx
+	.word 0x7ff
+	.word 0x7c00+gdt, 0
 
-load_msg:
-	.byte 13,10
-	.ascii "Load MOS in REAL ..."
-	.byte 13,10,13,10
-
-.text
-endtext:
-.data
-enddata:
-.bss
-endbss:
+.org 510
+	.word 0xAA55
