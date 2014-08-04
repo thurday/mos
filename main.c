68b44228c60ed5f34cc94abb639e73075a4dff11
范敦球 <fandunqiu@fandunqiu-desktop.(none)>
2014-08-04 15:49:10 +0800

    删除编译中间文件

--- main.c
+++ main.c
@@ -13,24 +13,24 @@ void* memset(void *dst, int c, uint n)
 	if ((int)dst%4 == 0 && n%4 == 0){
 		c &= 0xFF;
 		stosl(dst, (c<<24)|(c<<16)|(c<<8)|c, n/4);
-	} else
+	}else{
 		stosb(dst, c, n);
+	}
 	return dst;
 }
 
 void* memmove(void *dst, const void *src, uint n)
 { 
-	const char *s;
-	char *d;
+	const char *s = src;
+	char *d = dst;
 
-	s = src;
-	d = dst;
 	if(s < d && s + n > d){
 		s += n;
 		d += n;
-		while(n-- > 0)
+		while(n-- > 0){
 			*--d = *--s;
-	} else {
+		}
+	}else{
 		while(n-- > 0){
 			*d++ = *s++;
 		}
@@ -43,18 +43,20 @@ static void cgaputc(int c)
 {
 	int pos;
 
-	// Cursor position: col + 80*row.
 	outb(CRTPORT, 14);
 	pos = inb(CRTPORT+1) << 8;
 	outb(CRTPORT, 15);
 	pos |= inb(CRTPORT+1);
 
-	if(c == '\n')
+	if(c == '\n'){
 		pos += 80 - pos%80;
-	else if(c == BACKSPACE){
-		if(pos > 0) --pos;
-	} else
+	}else if(c == BACKSPACE){
+		if(pos > 0){
+			--pos;
+		}
+	}else{
 		crt[pos++] = (c&0xff) | 0x0700;  // black on white
+	}
 
 	if((pos/80) >= 24){  // Scroll up.
 		memmove(crt, crt+80, sizeof(crt[0])*23*80);
@@ -71,14 +73,14 @@ static void cgaputc(int c)
 
 int main(void)
 {
-
 	char* buf = "starting mos ...";
 	char* s = buf;
-	for(; *s; s++)
-	    cgaputc((*s) & 0xff);
+	for(; *s; s++){
+		cgaputc((*s) & 0xff);
+	}
 
-	while(1) ;
-
+	while(1) 
+		;
 	return 0;
 }
 
@@ -87,9 +89,9 @@ int main(void)
 // hence the "__aligned__" attribute.  
 // Use PTE_PS in page directory entry to enable 4Mbyte pages.
 __attribute__((__aligned__(PGSIZE)))
-pde_t entrypgdir[NPDENTRIES] = {
-  // Map VA's [0, 4MB) to PA's [0, 4MB)
-  [0] = (0) | PTE_P | PTE_W | PTE_PS,
-  // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
-  [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
-};
+	pde_t entrypgdir[NPDENTRIES] = {
+		// Map VA's [0, 4MB) to PA's [0, 4MB)
+		[0] = (0) | PTE_P | PTE_W | PTE_PS,
+		// Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
+		[KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
+	};
